ad6fb9c3c13894e2235a77cb7bc722402a3d3bc1
TrexGamerGirl <138319@student.bmg.vic.edu.au>
2025-12-08 14:54:25 +1100

    fixed 'n' conditional error

--- guess/high_low_guessing.py
+++ guess/high_low_guessing.py
@@ -1,5 +1,7 @@
 from random import randint
 cont = input("Do you wish to play my higher or lower game? y/n ")
+if cont == 'n':
+    print("I didn't want you to play anyway.")
 while cont == 'y':
      
     sec = randint(1,100)
@@ -18,8 +20,6 @@ while cont == 'y':
             print("HIGHER!")
         if true == sec:
             print("You Got it correct!")
-        cont = input("play again? y/n ")
-        if cont == 'n':
-            print("Thanks for Playing")
-if cont == 'n':
-    print("I didn't want you to play anyway.")
+            cont = input("play again? y/n ")
+            if cont == 'n':
+                print("Thanks for Playing")
