a763a7012254f9881ea2450d67a88fffca2d1539
TrexGamerGirl <138319@student.bmg.vic.edu.au>
2026-02-09 14:30:19 +1100

    EW Pady

--- Tkinter/tkinter_2.py
+++ Tkinter/tkinter_2.py
@@ -2,9 +2,19 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("Hello Tkinter")
+root.title("Hello")
 root.geometry("500x250")
 
-# TODO: add widgets using grid()
+
+frame = ttk.Frame(root, padding=12)
+frame.grid(row=0, column=0, sticky="NSEW")
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+frame.columnconfigure(1, weight=1)
+
+name_var = tk.StringVar()
+
+ttk.Label(frame, text ="Name:" ).grid(row=0, column=0, sticky="w", pady=6)
+ttk.Entry(frame, textvariable=name_var).grid(row=0, column=1, sticky="EW", pady=6)
 
 root.mainloop()
